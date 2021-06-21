ee74ec05c36748a5e2cfa9be10a57e283d1457c7
Alex Mozgovoy <milesAwayAlex@gmail.com>
2021-06-21 22:36:28 +0000

    Add assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✔️  Assertion Passed: ' + actual + '===' + expected);
+  } else {
+    console.log('🔴 Assertion Failed: ' + actual + '!==' + expected);
+  }
+};
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual('string', 'string');
+assertEqual(1,1);
+assertEqual(2, '2');
