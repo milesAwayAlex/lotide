2378d2b460d312c6bcd262aa998d4a6b54af44a0
Alex M <milesAwayAlex@gmail.com>
2021-06-28 15:27:25 -0700

    Use mocha and chai in head.test.js

--- test/head.test.js
+++ test/head.test.js
@@ -1,6 +1,22 @@
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 const head = require('../head.js');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([]), undefined);
+const cases = [
+  [[5, 6, 7], 5],
+  [['Hello', 'Lighthouse', 'Labs'], 'Hello'],
+  [[], undefined, 'an empty array'],
+];
+
+describe('#head', () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  cases.forEach(c => {
+    it(`returns ${c[1]} for ${c[2] || c[0]}`, () => {
+      assert.strictEqual(head(c[0]), c[1]);
+    });
+  });
+});
