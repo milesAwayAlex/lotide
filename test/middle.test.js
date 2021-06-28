06138fe526cf967e836d325d06376fdbe27fe4d7
Alex M <milesAwayAlex@gmail.com>
2021-06-28 16:27:13 -0700

    Use m&c for middle.test and tail.test

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,9 +1,20 @@
-const assertEqual = require('../assertEqual.js');
-const eqArrays = require('../eqArrays.js');
-
+const expect = require('chai').expect;
 const middle = require('../middle.js');
 
-assertEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
-assertEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
-assertEqual(eqArrays(middle([1, 2]), []), true);
-assertEqual(eqArrays(middle([]), []), true);
+const cases = [
+  [[1, 2, 3, 4, 5], [3]],
+  [
+    [1, 2, 3, 4, 5, 6],
+    [3, 4],
+  ],
+  [[1, 2], [], 'an empty array'],
+  [[], [], 'an empty array'],
+];
+
+describe('#middle', () => {
+  cases.forEach(c => {
+    it(`Returns ${c[2] || c[1]} for ${c[2] || c[0]}`, () => {
+      expect(middle(c[0])).eql(c[1]);
+    });
+  });
+});
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,9 +1,23 @@
-const assertEqual = require('../assertEqual.js');
+const expect = require('chai').expect;
 const tail = require('../tail.js');
 
 const originalArray = ['one', 'two', 'three'];
+const cases = [
+  [originalArray, ['two', 'three']],
+  [
+    ['Hello', 'Lighthouse', 'Labs'],
+    ['Lighthouse', 'Labs'],
+  ],
+  [[], [], 'an empty array'],
+];
 
-assertEqual(tail(originalArray), ['two', 'three']);
-assertEqual(originalArray.length, 3);
-assertEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
-assertEqual(tail([]), []);
+describe('#tail', () => {
+  cases.forEach(c => {
+    it(`Returns ${c[2] || c[1]} for ${c[2] || c[0]}`, () => {
+      expect(tail(c[0])).eql(c[1]);
+    });
+  });
+  it('Does not modify the original array', () => {
+    expect(originalArray).lengthOf(3);
+  });
+});
