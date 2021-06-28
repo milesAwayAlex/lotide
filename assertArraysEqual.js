d0f5830a64c9f7e27330c31da88f215b18addd5e
Alex M <milesAwayAlex@gmail.com>
2021-06-28 14:48:53 -0700

    Refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,10 @@
-const eqArrays = (a,b)=>a.every((e,i)=>e === b[i]);
+const eqArrays = require('./eqArrays.js');
 const assertArraysEqual = (actual, expected) => {
-  if (eqArrays(actual,expected)) {
+  if (eqArrays(actual, expected)) {
     console.log(`✔️  Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
